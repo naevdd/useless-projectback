25154918c1706680916a183a78dce6f2a7b262e8
R-Vaishnav-Raj <vaishnavraj029@gmail.com>
2024-11-02 22:33:55 +0530

    change route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 app.use(cors({ origin: "https://useless-projectfront.onrender.com" }));
 app.use(express.json());
 
